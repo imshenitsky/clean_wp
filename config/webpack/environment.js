c4f2c35cfacbcebe830342096121ab1a44d429a4
Alina Kovtun <alinakovtun11@gmail.com>
2021-09-02 20:37:06 +0300

    Bootstrap CSS, JS with Webpacker

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -5,8 +5,9 @@ const webpack = require('webpack')
 
 environment.plugins.append('Provide',
     new webpack.ProvidePlugin({
-        $: 'jquery',
-        jQuery: 'jquery'
+        $:      'jquery',
+        jQuery: 'jquery',
+        Popper: ['popper.js', 'default'] // Dependency of Bootstrap
     })
 )
 
